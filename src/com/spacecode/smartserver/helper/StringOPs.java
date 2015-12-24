92f9c965f506c07af9d2e1bfca2e3f3bc78883bd
nareshdavra <naresh0test@gmail.com>
2015-12-24 15:41:52 +0530

    just .. comment

--- src/com/spacecode/smartserver/helper/StringOPs.java
+++ src/com/spacecode/smartserver/helper/StringOPs.java
@@ -9,7 +9,7 @@ public final class StringOPs {
     public static String getCommaSaparetedString(Collection<String> allTagsnew){
         StringBuilder commaSepValueBuilder = new StringBuilder();
         for ( int i = 0; i< allTagsnew.size(); i++) {
-            //append the value into the builder ....
+            //append the value into the builder .....
             commaSepValueBuilder.append(allTagsnew.toArray()[i]);
             if (i != allTagsnew.size() - 1) {
                 commaSepValueBuilder.append(",");
